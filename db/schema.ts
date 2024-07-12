41eca232730b39888f824aa857b91873b40a7fd1
Jashan Mago <codinghashira@gmail.com>
2024-07-12 23:46:53 +0530

    added schema and seed data

--- db/schema.ts
+++ db/schema.ts
@@ -1,5 +1,12 @@
 import { relations } from "drizzle-orm";
-import { integer, pgTable, serial, text } from "drizzle-orm/pg-core";
+import {
+  boolean,
+  integer,
+  pgEnum,
+  pgTable,
+  serial,
+  text,
+} from "drizzle-orm/pg-core";
 
 export const courses = pgTable("courses", {
   id: serial("id").primaryKey(),
@@ -11,7 +18,125 @@ export const coursesRelations = relations(courses, ({ many }) => ({
   userProgress: many(userProgress, {
     relationName: "user-progress",
   }),
+  units: many(units, {
+    relationName: "units",
+  }),
 }));
+
+export const units = pgTable("units", {
+  id: serial("id").primaryKey(),
+  title: text("title").notNull(),
+  description: text("description").notNull(),
+  courseId: integer("course-id")
+    .references(() => courses.id, {
+      onDelete: "cascade",
+    })
+    .notNull(),
+  order: integer("order").notNull(),
+});
+
+export const unitsRelations = relations(units, ({ one, many }) => ({
+  course: one(courses, {
+    fields: [units.courseId],
+    references: [courses.id],
+  }),
+  lessons: many(lessons, {
+    relationName: "lessons",
+  }),
+}));
+
+export const lessons = pgTable("lessons", {
+  id: serial("id").primaryKey(),
+  title: text("title").notNull(),
+  unitId: integer("unit-id")
+    .references(() => units.id, {
+      onDelete: "cascade",
+    })
+    .notNull(),
+  order: integer("order").notNull(),
+});
+
+export const lessonsRelations = relations(lessons, ({ one, many }) => ({
+  unit: one(units, {
+    fields: [lessons.unitId],
+    references: [units.id],
+  }),
+  challenges: many(challenges, {
+    relationName: "challenges",
+  }),
+}));
+
+export const challengesEnums = pgEnum("type", ["SELECT", "ASSIST"]);
+
+export const challenges = pgTable("challenges", {
+  id: serial("id").primaryKey(),
+  title: text("title").notNull(),
+  type: challengesEnums("type").notNull(),
+  lessonId: integer("lesson-id")
+    .references(() => lessons.id, {
+      onDelete: "cascade",
+    })
+    .notNull(),
+  order: integer("order").notNull(),
+  question: text("question").notNull(),
+});
+
+export const challengesRelations = relations(challenges, ({ one, many }) => ({
+  lesson: one(lessons, {
+    fields: [challenges.lessonId],
+    references: [lessons.id],
+  }),
+  challengeOptions: many(challengeOptions, {
+    relationName: "challenge-options",
+  }),
+  challengeProgress: many(challengeProgress, {
+    relationName: "challenge-progress",
+  }),
+}));
+
+export const challengeOptions = pgTable("challenge-options", {
+  id: serial("id").primaryKey(),
+  challengeId: integer("challenge-id")
+    .references(() => challenges.id, {
+      onDelete: "cascade",
+    })
+    .notNull(),
+  text: text("text").notNull(),
+  correct: boolean("is-correct").notNull(),
+  imageSrc: text("image-src"),
+  audioSrc: text("audio-src"),
+});
+
+export const challengeOptionsRelations = relations(
+  challengeOptions,
+  ({ one }) => ({
+    challenge: one(challenges, {
+      fields: [challengeOptions.challengeId],
+      references: [challenges.id],
+    }),
+  })
+);
+
+export const challengeProgress = pgTable("challenge-progress", {
+  id: serial("id").primaryKey(),
+  challengeId: integer("challenge-id")
+    .references(() => challenges.id, {
+      onDelete: "cascade",
+    })
+    .notNull(),
+  userId: text("user-id").notNull(), // TODO: Confirm this doesn't break
+  completed: boolean("completed").notNull().default(false),
+});
+
+export const challengeProgressRelations = relations(
+  challengeProgress,
+  ({ one }) => ({
+    challenge: one(challenges, {
+      fields: [challengeProgress.challengeId],
+      references: [challenges.id],
+    }),
+  })
+);
 
 export const userProgress = pgTable("user-progress", {
   userId: text("user-id").primaryKey(),
--- scripts/seed.ts
+++ scripts/seed.ts
@@ -15,6 +15,11 @@ const main = async () => {
 
     await db.delete(schema.courses);
     await db.delete(schema.userProgress);
+    await db.delete(schema.units);
+    await db.delete(schema.lessons);
+    await db.delete(schema.challenges);
+    await db.delete(schema.challengeOptions);
+    await db.delete(schema.challengeProgress);
 
     await db.insert(schema.courses).values([
       {
@@ -37,6 +42,137 @@ const main = async () => {
         title: "Computer Science",
         imageSrc: "/programming.svg",
       },
+    ]);
+
+    await db.insert(schema.units).values([
+      {
+        id: 1,
+        title: "Algebra",
+        description: "Learn the basics of algebra",
+        courseId: 1,
+        order: 1,
+      },
+      {
+        id: 2,
+        title: "Geometry",
+        description: "Learn the basics of geometry",
+        courseId: 1,
+        order: 2,
+      },
+      {
+        id: 3,
+        title: "Calculus",
+        description: "Learn the basics of calculus",
+        courseId: 1,
+        order: 3,
+      },
+      {
+        id: 4,
+        title: "Differential Equations",
+        description: "Learn the basics of differential equations",
+        courseId: 1,
+        order: 4,
+      },
+      {
+        id: 5,
+        title: "Vector Calculus",
+        description: "Learn the basics of vector calculus",
+        courseId: 1,
+        order: 5,
+      },
+      {
+        id: 6,
+        courseId: 2,
+        title: "Mechanics I",
+        description: "Learn the basics of mechanics",
+        order: 1,
+      },
+      {
+        id: 7,
+        courseId: 2,
+        title: "Mechanics II",
+        description: "Learn the intermediates of mechanics",
+        order: 2,
+      },
+      {
+        id: 8,
+        courseId: 2,
+        title: "Electromagnetism",
+        description: "Learn the basics of electromagnetism",
+        order: 3,
+      },
+      {
+        id: 9,
+        courseId: 2,
+        title: "Fluid Mechanics",
+        description: "Learn the basics of fluid mechanics",
+        order: 4,
+      },
+      {
+        id: 10,
+        courseId: 2,
+        title: "Modern Physics",
+        description: "Learn the basics of modern physics",
+        order: 5,
+      },
+    ]);
+
+    await db.insert(schema.lessons).values([
+      {
+        id: 1,
+        title: "Introduction to Algebra",
+        unitId: 1,
+        order: 1,
+      },
+      { id: 2, unitId: 1, title: "Algebraic Operations", order: 2 },
+      { id: 3, unitId: 1, title: "Linear Equations", order: 3 },
+      { id: 4, unitId: 6, title: "Introduction to Mechanics", order: 1 },
+      { id: 5, unitId: 6, title: "Kinematics", order: 2 },
+      { id: 6, unitId: 6, title: "Dynamics", order: 3 },
+      { id: 7, unitId: 6, title: "Forces and Newton's Laws", order: 4 },
+      { id: 9, unitId: 6, title: "Circular Motion", order: 6 },
+      { id: 8, unitId: 6, title: "Work, Energy, and Power", order: 5 },
+    ]);
+
+    await db.insert(schema.challenges).values([
+      {
+        id: 1,
+        title: "Challenge 1",
+        type: "SELECT",
+        question:
+          "What is the standard unit of force in the International System of Units (SI)?",
+        lessonId: 4,
+        order: 1,
+      },
+      {
+        id: 2,
+        title: "Challenge 2",
+        type: "SELECT",
+        question:
+          "Which physical quantity describes the amount of matter in an object?",
+        lessonId: 4,
+        order: 2,
+      },
+      {
+        id: 3,
+        title: "Challenge 3",
+        type: "SELECT",
+        question:
+          "Which of the following is the unit of mass in the International System of Units (SI)?",
+        lessonId: 4,
+        order: 3,
+      },
+    ]);
+
+    await db.insert(schema.challengeOptions).values([
+      { id: 1, challengeId: 1, text: "N", correct: false },
+      { id: 2, challengeId: 1, text: "kg", correct: true },
+      { id: 3, challengeId: 1, text: "sec", correct: false },
+      { id: 4, challengeId: 1, text: "meter", correct: false },
+      { id: 5, challengeId: 2, text: "Mass", correct: true },
+      { id: 6, challengeId: 2, text: "Weight", correct: false },
+      { id: 7, challengeId: 2, text: "Force", correct: true },
+      { id: 8, challengeId: 2, text: "Velocity", correct: false },
     ]);
 
     console.log("Database seeded successfully");
